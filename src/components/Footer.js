92e28f95e5934d2f9fc05d292362855b0b6567ad
Uriel Nunez <uanq22@hotmail.com>
2022-06-23 23:49:03 -0500

    Navbar and Footer added and styled

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/css/Footer.css';
+
+function Footer() {
+    return (
+        <footer>
+            <p> Copyright 2022 JN Clean Sweep Inc. All rights reserved </p>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { Link } from 'react-router-dom';
+import '../assets/css/Navbar.css';
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+    return (
+        <nav className="navbar">
+            <div className="navbar-container">
+                <Link to="/" className="navbar-logo">
+                    JN Clean Sweep Inc
+                </Link>
+                <ul className="nav-menu">
+                    <li className="nav-item">
+                        <Link to='/about-us' className="nav-links">
+                            About Us
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/services' className="nav-links">
+                            Services
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/contact-us' className="nav-links">
+                            Contact Us
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
